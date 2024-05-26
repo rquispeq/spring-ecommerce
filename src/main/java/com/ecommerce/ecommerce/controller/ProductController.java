db8d42211034306171e4b5f3246818fb045abf95
Rony Quispe <oghmand@gmail.com>
2024-05-25 21:31:42 -0500

    feature update product

--- src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/ProductController.java
@@ -7,13 +7,16 @@ package com.ecommerce.ecommerce.controller;
 import com.ecommerce.ecommerce.model.Product;
 import com.ecommerce.ecommerce.model.User;
 import com.ecommerce.ecommerce.service.ProductService;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -47,6 +50,21 @@ public class ProductController {
         User user = new User(1, "", "", "", "", "", "", "");
         product.setUser(user);
         productService.save(product);
+        return "redirect:/products";
+    }
+    
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model){
+        Product product = new Product();
+        Optional<Product> optionalProduct = productService.get(id);
+        product = optionalProduct.get();
+        model.addAttribute("product", product);
+        return "products/edit";
+    }
+    
+    @PostMapping("/update")
+    public String update(Product product){
+        productService.update(product);
         return "redirect:/products";
     }
     
