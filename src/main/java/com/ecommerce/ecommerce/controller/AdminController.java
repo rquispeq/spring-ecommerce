5ff5d91c33aeeb2bc7b7f71268037c395ee032bf
Rony Quispe <oghmand@gmail.com>
2024-05-26 22:09:07 -0500

    show products for home page

--- src/main/java/com/ecommerce/ecommerce/controller/AdminController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/AdminController.java
@@ -4,7 +4,12 @@
  */
 package com.ecommerce.ecommerce.controller;
 
+import com.ecommerce.ecommerce.model.Product;
+import com.ecommerce.ecommerce.service.ProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,8 +22,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
     
+    @Autowired
+    private ProductService productService;
+    
     @GetMapping("")
-    public String home(){
+    public String home(Model model){
+        
+        List<Product> products = productService.getAll();
+        model.addAttribute("products", products);
+        
         return "admin/home";
     }
 }
