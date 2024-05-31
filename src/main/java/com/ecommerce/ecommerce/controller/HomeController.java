ea8fcc0e407c78b3e789bf857dd8c0bb475f6dad
Rony Quispe <oghmand@gmail.com>
2024-05-31 14:48:32 -0500

    feature to add products to cart

--- src/main/java/com/ecommerce/ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/ecommerce/controller/HomeController.java
@@ -4,7 +4,13 @@
  */
 package com.ecommerce.ecommerce.controller;
 
+import com.ecommerce.ecommerce.model.DetailOrder;
+import com.ecommerce.ecommerce.model.Order;
+import com.ecommerce.ecommerce.model.Product;
 import com.ecommerce.ecommerce.service.ProductService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +18,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -27,6 +35,10 @@ public class HomeController {
     @Autowired
     private ProductService productService;
     
+    List<DetailOrder> details = new ArrayList<DetailOrder>();
+    
+    Order order = new Order();
+    
     @GetMapping("")
     public String home(Model model){
         model.addAttribute("products", productService.getAll());
@@ -38,5 +50,20 @@ public class HomeController {
         
         model.addAttribute("product", productService.get(idProduct).get());
         return "user/producthome";
+    }
+    
+    @PostMapping("/cart")
+    public String addCart(@RequestParam Integer idProduct, @RequestParam Integer amount){
+        
+        DetailOrder detailOrder = new DetailOrder();
+        
+        Product product = new Product();
+        
+        double sumTotal = 0;
+        
+       
+        Optional<Product> productTarget = productService.get(idProduct);
+        
+        return "user/cart";
     }
 }
